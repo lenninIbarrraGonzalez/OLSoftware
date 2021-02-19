255e2011604ea04c5d193a73d7b0041e836fdb92
Lennin666 <soporte@allnexus.com.co>
2021-02-19 10:30:44 -0500

    implementando datos de prueba a la tabla

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -11,52 +11,84 @@ import TableRow from "@material-ui/core/TableRow";
 import Container from "@material-ui/core/Container";
 
 const columns = [
-  { id: "name", label: "Name", minWidth: 170 },
-  { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
+  { id: "firstName", label: "Nombres", minWidth: 100 },
+  { id: "lastName", label: "Apellidos", minWidth: 100 },
   {
-    id: "population",
-    label: "Population",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toLocaleString("en-US"),
+    id: "identification",
+    label: "Identificación (C.C)",
+    minWidth: 120,
   },
   {
-    id: "size",
-    label: "Size\u00a0(km\u00b2)",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toLocaleString("en-US"),
+    id: "role",
+    label: "Rol asociado",
+    minWidth: 80,
   },
   {
-    id: "density",
-    label: "Density",
+    id: "state",
+    label: "Estado",
+    minWidth: 50,
+  },
+  {
+    id: "phone",
+    label: "Teléfono",
+    minWidth: 100,
+  },
+  {
+    id: "email",
+    label: "Correo electrónico",
     minWidth: 170,
-    align: "right",
-    format: (value) => value.toFixed(2),
   },
 ];
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
+function createData(
+  firstName,
+  lastName,
+  identification,
+  role,
+  state,
+  phone,
+  email
+) {
+  return { firstName, lastName, identification, role, state, phone, email };
 }
 
 const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767),
+  createData(
+    "Andres Felipe",
+    "Garcia Castro",
+    1324171354,
+    "Administrador",
+    "Activo",
+    "3154415599",
+    "ing.lenninibarra@gmail.com"
+  ),
+  createData(
+    "Lennin Geovanny",
+    "Ibarra Gonzalez",
+    1403500365,
+    "Conductor",
+    "Activo",
+    "3154415599",
+    "ing.lenninibarra@gmail.com"
+  ),
+  createData(
+    "Natali Valentina",
+    "Martinez",
+    60483973,
+    "Recolector",
+    "Activo",
+    "3154415599",
+    "ing.lenninibarra@gmail.com"
+  ),
+  createData(
+    "Leidy Liliana",
+    "Burbano Galeano",
+    327167434,
+    "Digitador CG-UNO",
+    "Activo",
+    "3154415599",
+    "ing.lenninibarra@gmail.com"
+  ),
 ];
 
 const useStyles = makeStyles({
@@ -83,7 +115,7 @@ export default function Users() {
   };
 
   return (
-    <Container maxWidth="md">
+    <Container maxWidth="lg">
       <Paper className={classes.root}>
         <TableContainer className={classes.container}>
           <Table stickyHeader aria-label="sticky table">
