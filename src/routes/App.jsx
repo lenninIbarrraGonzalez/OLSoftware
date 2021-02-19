26075fd95a14f5aa119346f0a262f26ac20abfb9
Lennin666 <soporte@allnexus.com.co>
2021-02-19 12:46:03 -0500

    layout inicial

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -4,17 +4,20 @@ import Home from "../containers/Home";
 import NotFound from "../containers/NotFound";
 import AppContext from "../context/AppContext";
 import useInitialState from "../hooks/useinitiaState";
+import Layout from "../components/Layout";
 
 const App = () => {
   const initialState = useInitialState();
   return (
     <AppContext.Provider value={initialState}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
+      <Layout>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+      </Layout>
     </AppContext.Provider>
   );
 };
