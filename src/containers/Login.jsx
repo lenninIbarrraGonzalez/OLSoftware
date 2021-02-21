f01595a1f5c9344a3e1aa3c43301fffb79cc800c
Lennin666 <soporte@allnexus.com.co>
2021-02-20 20:25:47 -0500

    implementado diseño preliminar y boton

---
+++ src/containers/Login.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import { useHistory } from "react-router-dom";
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import TextField from "@material-ui/core/TextField";
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: 400,
+    padding: theme.spacing(5),
+    marginRight: theme.spacing(15)
+  },
+  container: {
+    height: "100vh",
+    width: "100vw",
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    background: "red"
+  },
+  title: {
+    display: "flex",
+    justifyContent: "center",
+    fontSize: 25,
+  }
+}));
+
+
+
+const Login = () => {
+
+  const classes = useStyles();
+  const history = useHistory();
+
+  const handleSubmit = () => {
+    history.push("/users");
+  }
+
+  return (
+    <>
+      <CssBaseline />
+      <div className={classes.container}>
+        <Card className={classes.root}>
+          <CardContent>
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+              Incio de sesión
+            </Typography>
+
+            <TextField
+              id="email"
+              name="email"
+              label="Usuario"
+              type="email"
+              style={{ margin: 8 }}
+              fullWidth
+              margin="normal"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+            />
+            <TextField
+              id="password"
+              name="password"
+              label="Contraseña"
+              type="password"
+              style={{ margin: 8 }}
+              fullWidth
+              margin="normal"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+            />
+            <Button fullWidth color="primary" variant="contained" onClick={handleSubmit}>Ingresar</Button>
+
+          </CardContent>
+
+        </Card>
+      </div>
+    </>
+  )
+}
+
+export default Login
